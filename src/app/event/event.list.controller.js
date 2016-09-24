1adbc8c7198ae3310529d660c64a7d286c0198d7
Lennon Jesus <lennon.jesus@gmail.com>
2016-09-24 20:44:25 -0300

    event list route and minor changes

---
+++ src/app/event/event.list.controller.js
@@ -0,0 +1,10 @@
+'use strict';
+
+angular.module('eventplanner').controller('EventList', eventList);
+
+/** @ngInject */
+function eventList() {
+
+  var vm = this;
+
+}
--- src/app/event/event.route.js
+++ src/app/event/event.route.js
@@ -10,6 +10,17 @@ function routesConfig($stateProvider) {
   $stateProvider
     .state('event', {
       url: '/event',
+      abstract: true,
+      template: '<ui-view/>'
+    })
+    .state('event.list', {
+      url: '/list',
+      templateUrl: 'app/event/event.list.html',
+      controller: 'EventList',
+      controllerAs: 'vm'
+    })
+    .state('event.create', {
+      url: '/create',
       templateUrl: 'app/event/event.form.html',
       controller: 'EventCreate',
       controllerAs: 'vm'
