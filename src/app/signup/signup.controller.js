2b493a436b6251b326c93137b08114fcd3e6639b
Lennon Jesus <lennon.jesus@gmail.com>
2016-09-25 14:15:49 -0300

    save user with localforage

--- src/app/signup/signup.controller.js
+++ src/app/signup/signup.controller.js
@@ -4,12 +4,19 @@ angular
   .module('eventplanner')
   .controller('Signup', signup);
 
-function signup() {
+/** @ngInject */
+function signup($localForage, $rootScope) {
   var vm = this;
 
   vm.createUser = createUser;
 
   function createUser(form) {
-
+    if (form.$valid) {
+      $localForage.setItem('user', vm.user).then(function () {
+        $localForage.getItem('user').then(function (data) {
+          $rootScope.loggedUser = data;
+        });
+      });
+    }
   }
 }
